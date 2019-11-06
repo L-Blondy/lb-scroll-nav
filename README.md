c3c0060c765a54006f2c9ec8832c335f876c127a
Laurent Blondy <laurent.blondy1@gmail.com>
2019-11-06 17:42:10 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Sroll Nav setup
+
+## Install 
+>npm install --save-dev lb-scroll-nav
+
+## Description
+- implements smooth scrool on modern browsers
+- updates the navbar state on scroll
+- removes # history spam
+
+## Usage
+index.html
+```
+<a href="#targetID">
+...
+<section id="targetID">
+```
+
+myfile.js
+```
+import setScrollNav from "lb-scroll-nav"
+...
+setScrollNav(sections, sectionsContainer, threshold)
+```
+sectionsContainer defaults to BODY
+threshold defaults to 0.5 => location will update when 50% of the section at least is displayed
---
+++ bundle.js
@@ -0,0 +1,32 @@
+export default function setScrollNav(sections, sectionsContainer = document.querySelector("body"), threshold = 0.5) {
+  document.addEventListener("click", e => {
+    const href = e.target.getAttribute("href");
+
+    if (href && e.target.getAttribute("href")[0] === "#") {
+      window.location.replace(href);
+    }
+  });
+  const options = {
+    threshold
+  };
+
+  function callback(entries) {
+    entries.forEach(entry => {
+      if (entry.intersectionRatio >= options.threshold) {
+        const id = entry.target.id;
+        const prevActive = document.querySelector(".navlink-active");
+        const newActive = document.querySelector(`[href="#${id}"]`);
+        prevActive && prevActive.classList.remove("navlink-active");
+        newActive.classList.add("navlink-active");
+        history.replaceState({}, id, `/#${id}`);
+      }
+    });
+  }
+
+  const observer = new IntersectionObserver(callback, options);
+  sections.forEach(section => {
+    observer.observe(section);
+  });
+  sectionsContainer.style.overflow = "scroll";
+  sectionsContainer.style.scrollBehavior = "smooth";
+}
